f2d9794ec356394a90fb29299fc68b9b85359984
Hs096 <46567261+Hs096@users.noreply.github.com>
2023-03-30 01:21:01 +0800

    Update ffd_json_test.js

--- ffd_json_test.js
+++ ffd_json_test.js
@@ -1,5 +1,5 @@
 // https://www.bejson.com 检测json格式、压缩json文本方便trn使用
-//指定ID实体画90度扇形
+//指定ID实体画90度扇形-顺劈
 {
 	"cmd": "add_omen",
 		"color": "enemy",
@@ -18,7 +18,7 @@
 	},
 	"duration": 2.5
 }
-//指定ID实体画圈 范围6.05
+//指定ID实体画圈 范围6.05-钢铁，分散
 {
 	"cmd": "add_omen",
 		"color": "enemy",
@@ -32,7 +32,35 @@
 	},
 	"duration": 2.5
 }
-//指定坐标画圈
+
+//指定ID实体画圈，并且随时间增加圈的大小-扩散特效
+{
+	"cmd": "add_omen",
+		"color": "enemy",
+			"shape_scale": {
+		"key": "circle",
+			"range": {
+			"key": "add",
+				"values": [3, {
+					"key": "add",
+					"values": [6, {
+						"key": "mul",
+						"values": [{
+							"key": "remain"
+						}, -1]
+					}]
+				}]
+
+
+		}
+	},
+	"pos": {
+		"key": "actor_pos",
+			"id": 1073923584
+	},
+	"duration": 2.5
+}
+//指定坐标画圈-AOE
 {
 	"cmd": "add_omen",
 		"color": "enemy",
@@ -43,7 +71,7 @@
 	"pos": [,,,],
 	"duration": 7
 }
-//对指定ID实体距离最远的实体画圈
+//对指定ID实体距离最远的实体画圈-地火引导
 {
 	"cmd": "add_omen",
 		"color": "enemy",
@@ -62,7 +90,70 @@
 	},
 	"duration": 7
 }
-//指定两个ID实体连线
+
+//对指定ID实体距离最远的实体画圈（且该目标不持有指定buff）-地火引导
+{
+	"cmd": "add_omen",
+		"color": "enemy",
+			"shape_scale": {
+		"key": "circle",
+			"range": 8
+	},
+	"pos": {
+		"key": "actor_pos",
+			"id": {
+			"key": "player_by_distance_idx",
+				"src": 1073923584,
+					"idx": {
+				"key": "if",
+					"cond": {
+					"key": "actor_has_status",
+						"id": {
+						"key": "player_by_distance_idx",
+							"src": 1073923584,
+								"idx": -1
+					},
+					"status_id": 1578
+				},
+				"false": -1,
+					"true": -3
+			}
+		}
+	},
+	"duration": 15
+}
+
+{
+	"cmd": "add_omen",
+		"color": "enemy",
+			"shape_scale": {
+		"key": "circle",
+			"range": 8
+	},
+	"pos": {
+		"key": "actor_pos",
+			"id": {
+			"key": "player_by_distance_idx",
+				"src": 1073923584,
+					"idx": {
+				"key": "if",
+					"cond": {
+					"key": "actor_has_status",
+						"id": {
+						"key": "player_by_distance_idx",
+							"src": 1073923584,
+								"idx": -1
+					},
+					"status_id": 1578
+				},
+				"false": -1,
+					"true": -3
+			}
+		}
+	},
+	"duration": 15
+}
+//指定两个ID实体连线-传毒
 {
 	"cmd": "add_line",
 		"color": "enemy",
@@ -77,7 +168,7 @@
 	"width": 30,
 	"duration": 10
 }
-//引导直线aoe，长度为两个指定实体的距离，面向为两个指定实体的相对面向
+//长度为两个指定实体的距离，面向为两个指定实体的相对面向的矩形-引导直线AOE
 {
 	"cmd": "add_omen",
 		"color": "enemy",
